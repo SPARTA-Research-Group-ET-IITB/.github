eeabb6f421b993f4a5df1f81048b5bfb079675d9
Kartikay Agrawal <89684855+kartikay24@users.noreply.github.com>
2023-04-06 11:49:59 +0530

    Create extract_website_content.py

---
+++ extract_website_content.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+# URL of the website to scrape
+url = 'https://example.com'
+
+# Request the website content
+response = requests.get(url)
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Extract the desired content from the HTML using BeautifulSoup
+content = soup.find_all('p')
+
+# Create a string with the extracted content
+content_str = ''
+for paragraph in content:
+    content_str += str(paragraph) + '\n'
+
+# Write the content to a README.md file in the GitHub repository
+with open('README.md', 'w') as f:
+    f.write(content_str)
+
+# Add and commit the changes to the GitHub repository
+os.system('git add README.md')
+os.system('git commit -m "Update README.md with latest content"')
+os.system('git push')
