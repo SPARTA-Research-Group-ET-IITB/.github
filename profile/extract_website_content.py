236702c3c04049b3bb759e3ba745fd35ffc8f040
Kartikay Agrawal <89684855+kartikay24@users.noreply.github.com>
2023-04-11 13:14:29 +0530

    changed current directory

--- profile/extract_website_content.py
+++ profile/extract_website_content.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+# Change directory to root of the repository
+os.chdir('SPARTA-Research-Group-ET-IITB/.github/profile')
+
 # URL of the website to scrape
 url = 'https://syaamantak-das.carrd.co/'
 
